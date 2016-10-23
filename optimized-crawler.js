25f199565b0d098298b80485176713943968c0a7
CaiYiLiang <877526529@qq.com>
2016-10-23 16:34:10 +0800

    optimized-crawler.js can crawler muti-pages

--- optimized-crawler.js
+++ optimized-crawler.js
@@ -13,19 +13,18 @@ var Promise = require("promise"); //ES6 buit-in object
 var cheerio = require("cheerio");
 var baseUrl = "http://www.imooc.com/learn/";
 // var url ="http://www.imooc.com/learn/348";
-var videoIds = [348,637,259,75,97];
+var videoIds = [348,637,259,75,197];
 
 var courseArray = [];
-
 videoIds.forEach(function(courseId){
 	courseArray.push(getPageAsync(baseUrl+courseId));
 });
 
 
-
 function getPageAsync(url){
+	
 	return new Promise(function(resolve,reject){
-		console.log("It is crawlering...");
+		 console.log("It is crawlering...");
         
         http.get(url,function(res){
 			var html = "";
@@ -48,43 +47,41 @@ function getPageAsync(url){
 	});
 };
 
-
 // Promise.all(promiseArray) is to make every promise object becoming 'slove' status
 // When each promise object(every element in promiseArray) goes into reslove/reject status , 
 // then return the value-array by exute order
 
 //Main()
 Promise.all(courseArray).then(function onFulfilled(pages){
-	 console.log("promise resolve callbackFn.");
-     var courseData = [];
+	
+     //var coursesData = [];
 
      pages.forEach(function(html){
-     	var course = filterChapters(html);
-     	courseData.push(course);
-        console.log("pages.forEach");
-     	console.log("courseData - printInfo.");
-        printInfo(courseData);
+     	// console.log("pages.forEach");
+     	//var coursePkg = filterChapters(html);
+     	//coursesData.push(coursePkg);
+     	// console.log("coursesData - printInfo.");
+    	printInfo(filterChapters(html));     	
      });
+
      
 }, function onRejected(e){
     console.log(e);
 });
 
-
-
 function filterChapters(html){
-	console.log("filterChapters(html).");
+	// console.log("filterChapters(html).");
 	var $ = cheerio.load(html);
 	var chapters = $(".chapter");
     var title = $(".course-infos .path span").text();
     
-    console.log("title:"+title);
+    // console.log("title:"+title);
 	var courseData = {
 		title:title,
 		course:[]  //define an array name 'course' with no elements
 	} ; 
     
-     console.log("chapters.each======"+courseData.title);
+     // console.log("chapters.each======"+courseData.title);
 	chapters.each(function(index) {
 		var chapter = $(this); 
 		// current index object : chapters[index]
@@ -95,40 +92,48 @@ function filterChapters(html){
         	videos : []
         };
         
-        console.log("videos.each======");
+        // console.log("videos.each======");
         videos.each(function(item){ 
             var video = $(this).find('.J-media-item');
             var videoId = video.attr('href').split('video/')[1]; 
             var videoTitle = video.text();
-            console.log("videos.each==ing==");
+            // console.log("videos.each==ing==");
 
             chapterData.videos.push({  //array keyandvalue , Object.keys
             	title : videoTitle ,
             	id : videoId
             });
-             console.log("videos:"+videoId.trim()+" videoTitle"+videoTitle.trim());
+             // console.log("videos:"+videoId.trim()+" videoTitle"+videoTitle.trim());
         });
         
-        console.log("course.push");
-        course.push(chapterData);
+        // console.log("course.push");
+        courseData.course.push(chapterData);
 	}); 
-        
-  	    return course;
+        // console.log("return course");
+  	    return courseData;
 };
 
-function printInfo(courses){
-	    console.log("printInfo(courses).");
-        courses.forEach(function(currentCourse){
-		var chapterTitle = currentCourse.title;
-		// console.log(chapterTitle.trim()+"\n");
+function printInfo(coursePkg){
+	    // console.log("printInfo(coursePkg).");
+	    console.log("=========printInfo BEGIN=========");
+        
+        var courseTitle = coursePkg.title;
+		// console.log("courseTitle");
+		console.log(courseTitle.replace(/\s+/g,"")+"\n");
+
+        coursePkg.course.forEach(function(currentCourse){
+		var chapterTitle = currentCourse.chapterTitle;
+		// console.log("coursePkg.course");
 		console.log(chapterTitle.replace(/\s+/g,"")+"\n");
         
-		var videos = currentCourse.course;
-        videos.forEach(function(video){
-        	// console.log("【"+ video.id.trim() +"】"+video.title.trim() +"\n")
+        currentCourse.videos.forEach(function(video){
+        // console.log("【"+ video.id.trim() +"】"+video.title.trim() +"\n")
+
         	console.log("【"+ video.id.replace(/\s+/g,"") +"】"+video.title.replace(/\s+/g,"") +"\n")
         });
 	});
+        console.log("=========printInfo DONE=========");
+        console.log("\n");
 }
 
 

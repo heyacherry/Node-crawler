ad9c243793b7eb67f3833df622dffa0f02405e75
CaiYiLiang <877526529@qq.com>
2016-10-30 23:43:07 +0800

    update display format

--- douban-popular-movie.js
+++ douban-popular-movie.js
@@ -5,13 +5,35 @@ Story : crawl the top20/40 popular movies from douban.movie.com
 "use strict";
 
 var https = require("https");
-
-var url = "https://movie.douban.com/explore#!type=movie&tag=%E7%83%AD%E9%97%A8&sort=rank&page_limit=20&page_start=0";
-
+var cheerio = require("cheerio");
+var url = "https://movie.douban.com/top250";
 
 // ES6 Arrow function
 https.get(url, res=> {  
-  console.log("响应：" + res.statusCode);
-}).on('error', function(e) {
+  console.log('statusCode:', res.statusCode);
+  var html ="";
+
+  res.on('data', data => {
+  	 html+=data
+  });
+
+  res.on("end", ()=>{
+  filterContent(html);
+  //printInfo();
+  });
+  
+}).on('error', e=> {
   console.log("错误：" + e.message);
 });
+
+
+function filterContent(html){
+  var $ = cheerio.load(html);
+  var movies = $(".article ol").children('li');
+  var movieObjs = [];
+  //console.log("movies.length：" + movies.length);
+  /***
+  movieObj contains:
+  
+  ***/
+}

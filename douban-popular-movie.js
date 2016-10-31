a8ec92fa186f3318f778e5f820cca01680046db9
CaiYiLiang <877526529@qq.com>
2016-10-31 23:21:07 +0800

    update log format

--- douban-popular-movie.js
+++ douban-popular-movie.js
@@ -1,22 +1,17 @@
 /***********
-Story : crawl the top20/40 popular movies from douban.movie.com
-
+Story : crawl the top25/50 popular movies from douban.movie.com
 ************/
 "use strict";
 
 var https = require("https");
-<<<<<<< HEAD
 var cheerio = require("cheerio");
 var url = "https://movie.douban.com/top250";
-=======
-
-var url = "https://movie.douban.com/top250";
-
->>>>>>> e3e4efb7e19ecd2b4532b2d50c6939928bbf1321
 
 // ES6 Arrow function
 https.get(url, res=> {  
   console.log('statusCode:', res.statusCode);
+  res.setEncoding('utf-8');
+
   var html ="";
 
   res.on('data', data => {
@@ -31,18 +26,28 @@ https.get(url, res=> {
 }).on('error', e=> {
   console.log("错误：" + e.message);
 });
-<<<<<<< HEAD
 
 
 function filterContent(html){
+  //console.log(html);
   var $ = cheerio.load(html);
   var movies = $(".article ol").children('li');
   var movieObjs = [];
-  //console.log("movies.length：" + movies.length);
+  console.log("movies.length：" + movies.length );
   /***
   movieObj contains:
-  
+  {moveName , country, year , descrition, score and display right tag}
   ***/
+  movies.each(function(index) {
+  	 var movie = $(this);
+  	 var moveNames = movie.find(".title").text();
+     var playable = movie.find(".playable").text();
+     var start = movie.find(".rating_num").text();
+     var descrition = movie.find(".inq").text();
+
+  	 console.log("-- "+moveNames + " ["+ start +"] " +"\n");
+  	  console.log("   "+descrition + playable +"\n");
+  	
+  });
+   
 }
-=======
->>>>>>> e3e4efb7e19ecd2b4532b2d50c6939928bbf1321
